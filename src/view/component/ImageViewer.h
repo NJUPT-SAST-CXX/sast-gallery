ba571a64a2bdab792c2e014e35c10653d7b16db6
Ye Zhenrong <3471626272@qq.com>
2025-02-23 17:39:05 +0800

    refactor(ImageViewer): Remove commented-out legacy code and simplify implementation

--- src/view/component/ImageViewer.h
+++ src/view/component/ImageViewer.h
@@ -1,9 +1,6 @@
 #pragma once
 
 #include "AbstractMediaViewer.h"
-// #include <QGraphicsPixmapItem>
-// #include <QGraphicsView>
-// #include <QMouseEvent>
 
 class ImageViewer : public AbstractMediaViewer {
     Q_OBJECT
@@ -11,35 +8,4 @@ class ImageViewer : public AbstractMediaViewer {
 public:
     explicit ImageViewer(QWidget* parent = nullptr);
     explicit ImageViewer(const QPixmap& pixmap, QWidget* parent = nullptr);
-    // void setContent(const QPixmap& pixmap, bool fadeAnimation = true);
-    // void setContent(const QImage& image, bool fadeAnimation = true);
-
-    // [[nodiscard]] int getScale() const;
-    // [[nodiscard]] int getMinScale() const;
-    // [[nodiscard]] int getMaxScale() const;
-    // void setMinScale(int scale);
-    // void setMaxScale(int scale);
-    // void scaleTo(int scale);
-
-signals:
-    // void wheelScrolled(int delta);
-    // void resized();
-
-protected:
-    // void mousePressEvent(QMouseEvent* event) override;
-    // void mouseMoveEvent(QMouseEvent* event) override;
-    // void mouseReleaseEvent(QMouseEvent* event) override;
-    // void resizeEvent(QResizeEvent* event) override;
-    // void wheelEvent(QWheelEvent* event) override;
-
-private:
-    // QGraphicsScene* scene;
-    // QGraphicsPixmapItem* pixmapItem;
-    // QPoint lastMousePos;
-    // bool dragging;
-    // int cntScale;
-    // int minScale;
-    // int maxScale;
-
-    // void adjustImageToFit();
 };
